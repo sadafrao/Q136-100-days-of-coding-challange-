f5b9a0137d8b84ddf30c3b8429c4b1191bbeae27
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-16 17:55:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// 136 Day 
+// use  console.log inside a loop to track the variables
+for (var i = 0; i < 10; i++) {
+    console.log("".concat(i, ",i value: , i "));
+}
---
+++ index.ts
@@ -0,0 +1,5 @@
+// 136 Day 
+// use  console.log inside a loop to track the variables
+for (let i = 0; i < 10; i++) {
+    console.log(`${i},i value: , i `)
+}
